0e3463a16ceb3a63a7add101413cee555c5e7f89
BardoBardo <nigelhois47@gmail.com>
2022-09-03 21:52:13 +0200

    filter for space, upper and quotes

--- Modes/Shared.hs
+++ Modes/Shared.hs
@@ -1,11 +1,8 @@
 module Modes.Shared where
 
-import Data.List ( elemIndex ) 
-import System.Console.ANSI
-
 --my libs
 
-import Basic ( replaceAtIndex, iterateFunc, badImul', naturalPos )
+import Basic ( replaceAt, iterateFunc, badImul', naturalPos )
 import Binary.Shift ( shiftBinL32, shiftBinR32 )
 import Binary.Logicgates ( xorB' )
 import Binary.Conversion ( toDec, decToBase )
@@ -37,48 +34,7 @@ sum' x y z
 
 getIndex seed items sum = reduceWeight items $ range 1 sum seed
 
-replaceAtIndex' :: (Eq a1, Num a1, Num a2) => a1 -> Int -> [a2] -> [a2]
-replaceAtIndex' x x2 list
+replaceAt' :: (Eq a1, Num a1, Num a2) => a1 -> Int -> [a2] -> [a2]
+replaceAt' x x2 list
     | x == (-1) = list
-    | otherwise = replaceAtIndex x2 0 list
-
-findIndex' :: String -> String -> [String] -> IO Int
-findIndex' name msg list = do
-    if null name
-        then
-            return (-1)
-        else do
-            let y = elemIndex name list
-            case y of
-                Just x -> return x
-                Nothing -> do
-                    print "ERROR: Wrong input"
-                    print msg
-                    x <- getLine :: IO String
-                    findIndex' x msg list
-
-findIndexWithError :: String -> String -> [String] -> IO Int
-findIndexWithError name msg list = do
-    let y = elemIndex name list
-    case y of
-        Just x -> return x
-        Nothing -> do
-            print "ERROR: Wrong input"
-            print msg
-            x <- getLine :: IO String
-            findIndexWithError x msg list
-
-
-nextRoom :: String -> [String] -> IO Int
-nextRoom room list = do
-    clearScreen
-
-    putStr "Pick one of the following: "
-    putStr "\n\n"
-    print list
-    putStr "\n"
-
-    print $ room ++ ": "
-    x <- getLine :: IO String
-
-    findIndex' x (room ++ ": ") list
+    | otherwise = replaceAt x2 0 list
